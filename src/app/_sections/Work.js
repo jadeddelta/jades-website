c6a5502a420a992e140bfc1cb92e44650595e010
jade <101148768+jadeddelta@users.noreply.github.com>
2024-08-26 10:15:31 -0700

    final polish

--- src/app/_sections/Work.js
+++ src/app/_sections/Work.js
@@ -2,7 +2,7 @@ import Timeline from "../_components/work/Timeline";
 
 export default function Work() {
     return (
-        <div className="flex flex-col items-center justify-evenly w-full py-16 gap-16 border border-emerald-300" id="work">
+        <div className="flex flex-col items-center justify-evenly w-full pt-16 pb-8 gap-24" id="work">
             <p className="text-4xl">
                 professional experience
             </p>
