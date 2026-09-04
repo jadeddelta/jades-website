a3910a3e22774517d821a622ab3cf9020a97ec45
jade <jadeddelta@gmail.com>
2026-09-04 15:25:31 -0400

    set up typescript toolchain

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,10 @@
 import next from 'eslint-config-next/core-web-vitals'
+import nextTypescript from 'eslint-config-next/typescript'
 
 const config = [
   { ignores: ['.next/**', 'out/**'] },
   ...next,
+  ...nextTypescript,
 ]
 
 export default config
